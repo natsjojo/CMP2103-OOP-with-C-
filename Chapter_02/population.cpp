d5bb5ad93d6dbe3363e5f1367e6f5d4e37e96002
natsjojo <262077697+natsjojo@users.noreply.github.com>
2026-09-08 13:16:31 +0300

    Population and area of triangle assignments

--- Chapter_02/population.cpp
+++ Chapter_02/population.cpp
@@ -6,3 +6,20 @@
 // Write a program to display the population for each of the next five years. Assume the current population is 312032486 and one year has 365 days.
 
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long currentPopulation = 312032486;
+    int secondsInAYear = 365 * 24 * 60 * 60;
+    int births = secondsInAYear / 7;// Number of births per year
+    int deaths = secondsInAYear / 13;// Number of deaths per year
+    int immigrants = secondsInAYear / 45;// Number of immigrants per year
+
+    for (int year = 1; year <= 5; year++) {
+        currentPopulation += (births - deaths + immigrants);
+        cout << "Population after " << year << " year(s): " << currentPopulation << endl;
+    }
+
+    return 0;
+}
